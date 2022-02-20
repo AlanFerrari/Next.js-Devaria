d3d727537d9606a24950ecf392dc4ca9bf4cb725
alanferrari <alanferrari33@gmail.com>
2022-02-19 21:32:53 -0300

    início da criptografia com token

--- devagram-node-nextjs/pages/api/login.ts
+++ devagram-node-nextjs/pages/api/login.ts
@@ -3,19 +3,33 @@ import {conectarMongoDB} from '../../middlewares/conectarMongoDB';
 import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
 import md5 from 'md5';
 import { UsuarioModel } from '../../models/UsuarioModel';
+import jwt from 'jsonwebtoken';
+import type { LoginResposta } from '../../types/LoginResposta';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 const endpointLogin = async (
     req: NextApiRequest,
-    res: NextApiResponse<RespostaPadraoMsg>
+    res: NextApiResponse<RespostaPadraoMsg | LoginResposta>
 ) => {
+
+    const {MINHA_CHAVE_JWT} = process.env;
+    if (!MINHA_CHAVE_JWT) {
+        return res.status(500).json({erro: 'ENV Jwt não foi informada'});
+    }
+
     if(req.method === 'POST'){
         const {login, senha} = req.body;
 
         const usuariosEncontrados = await UsuarioModel.find({email: login, senha: md5(senha)});
         if(usuariosEncontrados && usuariosEncontrados.length > 0) {
             const usuarioEncontrado = usuariosEncontrados[0];
-            return res.status(200).json({msg: `Usuário ${usuarioEncontrado.nome} autenticado com sucesso!`});
+            
+            const token = jwt.sign({_id: usuarioEncontrado._id}, MINHA_CHAVE_JWT);
+            return res.status(200).json({
+                nome: usuarioEncontrado.nome,
+                email: usuarioEncontrado.email,
+                token
+            });
         }
         return res.status(400).json({erro: 'Usuário ou senha inválido!'});
     }
