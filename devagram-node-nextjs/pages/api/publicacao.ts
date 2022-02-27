a42172e51bdaba2a7a54124a85bb3e6e54a9c5d2
alanferrari <alanferrari33@gmail.com>
2022-02-27 19:42:12 -0300

    endpoint publicação

---
+++ devagram-node-nextjs/pages/api/publicacao.ts
@@ -0,0 +1,59 @@
+import type {NextApiResponse} from 'next';
+import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
+import nc from 'next-connect';
+import { upload, uploadImageCosmic } from '../../services/uploadImageCosmic';
+import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
+import { validarTokenJWT } from '../../middlewares/validarTokenJWT';
+import { PublicacaoModel } from '../../models/PublicacaoModel';
+import { UsuarioModel } from '../../models/UsuarioModel';
+
+const handler = nc()
+    .use(upload.single('file'))
+    .post(async (req: any, res: NextApiResponse<RespostaPadraoMsg>) => {
+
+        try {
+            const {userId} = req.query;
+            const usuario = await UsuarioModel.findById(userId);
+            if(!usuario){
+                return res.status(400).json({erro: 'Usuário não encontrado.'});
+            }
+
+
+            if(!req || !req.body){
+                return res.status(400).json({erro: 'Parametros não informados'});
+
+            }
+            const {descricao} = req?.body;
+
+            if(!descricao || descricao.length < 2){
+                return res.status(400).json({erro: 'Descrição inválida'});
+            }
+            if (!req.file || !req.file.originalname) {
+                return res.status(400).json({erro: 'Imagem é obrigatória'});
+            }
+
+            const image = await uploadImageCosmic(req);
+            const publicacao = {
+                idUsuario: usuario._id,
+                descricao,
+                foto: image.media.url,
+                data: new Date()
+            }
+
+            await PublicacaoModel.create(publicacao);
+            return res.status(200).json({msg: 'Publicação feita com sucesso!'});
+                
+        } catch (e) {
+            console.log(e);
+            return res.status(400).json({erro: 'Erro ao fazer a publiicação.'});
+        }
+        
+    }); 
+
+    export const config = {
+        api: {
+            bodyParser: false
+        }
+    }
+
+    export default validarTokenJWT(conectarMongoDB(handler));
--- devagram-node-nextjs/services/uploadImageCosmic.ts
+++ devagram-node-nextjs/services/uploadImageCosmic.ts
@@ -24,9 +24,8 @@ const upload = multer({storage: storage});
 const uploadImageCosmic = async (req: any) => {
     if(req?.file?.originalname){
 
-        if(!req.file.originalname.includes('.png') &&
-        !req.file.originalname.includes('.jpg') &&
-        !req.file.originalname.includes('.jpeg')){
+        if(!req.file.originalname.includes('.png') && !req.file.originalname.includes('.jpg') && !req.file.originalname.includes('.jpeg')){
+            
             throw new Error('Extensão da imagem inválida!');
         }
 
