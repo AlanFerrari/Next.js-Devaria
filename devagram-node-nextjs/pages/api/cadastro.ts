6caefc4d50e54c6f2db1a6d80801e795eba474ef
alanferrari <alanferrari33@gmail.com>
2022-02-26 23:17:45 -0300

    upload de imagens

--- devagram-node-nextjs/pages/api/cadastro.ts
+++ devagram-node-nextjs/pages/api/cadastro.ts
@@ -4,41 +4,59 @@ import type {CadastroRequisicao} from '../../types/CadastroRequisicao';
 import {UsuarioModel} from '../../models/UsuarioModel';
 import {conectarMongoDB} from '../../middlewares/conectarMongoDB';
 import md5 from 'md5';
+import {upload, uploadImageCosmic} from '../../services/uploadImageCosmic';
+import nc from 'next-connect';
 
-const endpointCadastro = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
+const handler = nc()
+    .use(upload.single('file'))
+    .post(async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
+           
+       try {
+            const usuario = req.body as CadastroRequisicao;
+            
+            if(!usuario.nome || usuario.nome.length < 3){
+                return res.status(400).json({erro: `Nome inválido!`});
+            }
+            
+            if (!usuario.email || usuario.email.length < 6 || !usuario.email.includes('@') || !usuario.email.includes('.')) {
+                return res.status(400).json({erro: `E-mail inválido!`});            
+            }
+            
+            if (!usuario.senha || usuario.senha.length < 8) {
+                return res.status(400).json({erro: `Senha inválida!`});            
+            }
 
-    if(req.method === 'POST'){
-        const usuario = req.body as CadastroRequisicao;
-        
-        if(!usuario.nome || usuario.nome.length < 3){
-            return res.status(400).json({erro: `Nome inválido!`});
-        }
-        
-        if (!usuario.email || usuario.email.length < 6 || !usuario.email.includes('@') || !usuario.email.includes('.')) {
-            return res.status(400).json({erro: `E-mail inválido!`});            
-        }
-        
-        if (!usuario.senha || usuario.senha.length < 8) {
-            return res.status(400).json({erro: `Senha inválida!`});            
-        }
+            //Validação de email, verificando se já existe um usuário com o mesmo email
+            const usuarioComMesmoEmail = await UsuarioModel.find({email: usuario.email});
+            if (usuarioComMesmoEmail && usuarioComMesmoEmail.length > 0) {
+                return res.status(400).json({erro: 'Já existe uma conta com este email'});
+            }
 
-        //Validação de email, verificando se já existe um usuário com o mesmo email
-        const usuarioComMesmoEmail = await UsuarioModel.find({email: usuario.email});
-        if (usuarioComMesmoEmail && usuarioComMesmoEmail.length > 0) {
-            return res.status(400).json({erro: 'Já existe uma conta com este email'});
-        }
+            //Enviar a imagem do multer para o cosmic
+            const image = await uploadImageCosmic(req);
 
-        //Salvar os dados no banco de dados
-        const usuarioASerSalvo = {
-            nome: usuario.nome,
-            email: usuario.email,
-            senha: md5(usuario.senha)
-        }
+            //Salvar os dados no banco de dados
+            const usuarioASerSalvo = {
+                nome: usuario.nome,
+                email: usuario.email,
+                senha: md5(usuario.senha),
+                avatar: image?.media?.url
+            }
 
-        await UsuarioModel.create(usuarioASerSalvo);
-        return res.status(200).json({msg: 'Usuário cadastrado com sucesso!'}); 
+            await UsuarioModel.create(usuarioASerSalvo);
+            return res.status(200).json({msg: 'Usuário cadastrado com sucesso!'});
+       } catch (e: any) {
+           console.log(e);
+           return res.status(400).json({erro: e.toString()});
+       }
+         
+    });
+
+export const config ={
+    api: {
+        bodyParser: false  
     }
-    return res.status(405).json({erro: 'Método informado inválido!'});
 }
 
-export default conectarMongoDB(endpointCadastro);
+
+export default conectarMongoDB(handler);
---
+++ devagram-node-nextjs/services/uploadImageCosmic.ts
@@ -0,0 +1,47 @@
+import multer from 'multer';
+import cosmicjs from 'cosmicjs';
+
+const{
+    CHAVE_GRAVACAO_AVATARES,
+    CHAVE_GRAVACAO_PUBLICACOES,
+    BUCKET_AVATARES,
+    BUCKET_PUBLICACOES} = process.env;
+
+const Cosmic = cosmicjs();
+const bucketAvatares = Cosmic.bucket({
+    slug: BUCKET_AVATARES,
+    write_key: CHAVE_GRAVACAO_AVATARES
+});
+
+const bucketPuclicacoes = Cosmic.bucket({
+    slug: BUCKET_PUBLICACOES,
+    write_key: CHAVE_GRAVACAO_PUBLICACOES
+});
+
+const storage = multer.memoryStorage();
+const upload = multer({storage: storage});
+
+const uploadImageCosmic = async (req: any) => {
+    if(req?.file?.originalname){
+
+        if(!req.file.originalname.includes('.png') &&
+        !req.file.originalname.includes('.jpg') &&
+        !req.file.originalname.includes('.jpeg')){
+            throw new Error('Extensão da imagem inválida!');
+        }
+
+        const media_object = {
+            originalname: req.file.originalname,
+            buffer: req.file.buffer
+        };
+
+        if(req.url && req.url.includes('publicacao')){
+            return await bucketPuclicacoes.addMedia({media: media_object});
+        }
+        else{
+            return await bucketAvatares.addMedia({media: media_object});
+        }
+    }
+}
+
+export {upload, uploadImageCosmic};
